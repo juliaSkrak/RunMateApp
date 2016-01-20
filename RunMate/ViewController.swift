85996c97305aa4b0c04982efb17034cab6e216d2
juliaSkrak <julia.skrak@gawker.com>
2016-01-19 22:41:54 -0500

    Initial Commit

---
+++ RunMate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RunMate
+//
+//  Created by Julia Skrak on 1/19/16.
+//  Copyright © 2016 skrakattack. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
